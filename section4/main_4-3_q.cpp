5e429479feb61512e5129bc4fa0874f373259e0b
SeungYeol Lee <stl990726@gmail.com>
2025-05-01 14:52:37 +0900

    4-3 commit

---
+++ section4/main_4-3_q.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+
+
+class string {
+    char *str;
+    int len;
+  
+   public:
+    string(char c, int n);  // 문자 c 가 n 개 있는 문자열로 정의
+    string(const char *s);
+    string(const string &s);
+    ~string();
+  
+    void add_string(const string &s);   // str 뒤에 s 를 붙인다.
+    void copy_string(const string &s);  // str 에 s 를 복사한다.
+    int strlen();                       // 문자열 길이 리턴
+  };
+string::string(char c, int n){
+    for (int i=0;i<n;i++){
+
+    }
+
+}
+string::string(const char *s){
+
+}
+string::string(const string &s){
+
+}
+string::~string(){
+    
+}
+
+
+int main(){
+    string string('e',5);
+    return 0;
+}
